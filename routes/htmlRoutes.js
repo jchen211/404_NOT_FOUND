ea00c37f9cd6caf255e5da2853cf1c767396aa65
Joe Schmitz <josephschmitz2018@u.northwestern.edu>
2019-03-27 18:42:20 -0500

    javascript routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,18 @@
 var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load formlisting page
+  app.get("/formlisting", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/formlisting.html"));
+  });
+  // Load search page
+  app.get("/search", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/search.html"));
   });
 
   // Render 404 page for any unmatched routes
