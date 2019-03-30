c657ef29adf43652c7993a93b5ff817294afa0a8
Joe Schmitz <josephschmitz2018@u.northwestern.edu>
2019-03-29 19:04:42 -0500

    added time page for dev

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,22 +1,26 @@
 var db = require("../models");
 var path = require("path");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
   // Load formlisting page
-  app.get("/formlisting", function(req, res) {
+  app.get("/formlisting", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/formlisting.html"));
   });
+  // Load formtime page
+  app.get("/formtime", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/formtime.html"));
+  });
   // Load search page
-  app.get("/search", function(req, res) {
+  app.get("/search", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/search.html"));
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 var express = require("express");
 // var exphbs = require("express-handlebars");
-var { DateTime } = require("luxon");
+var {
+  DateTime
+} = require("luxon");
 DateTime.local();
 var db = require("./models");
 
@@ -9,7 +11,9 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
@@ -28,7 +32,9 @@ require("./routes/times-apiRoutes")(app);
 require("./routes/property-apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+var syncOptions = {
+  force: false
+};
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
@@ -37,8 +43,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
